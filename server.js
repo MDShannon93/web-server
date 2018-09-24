5ac39c13832e646d872216f7768ce2b98fa480a5
Michael Shannon <michael.david.shannon@ibm.com>
2018-09-24 18:35:38 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>')
+    res.render('welcome.hbs' , {
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Welcome to Michael\'s website'
+    });
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad link used'
+    })
+});
+
+app.listen(2000, () => {
+    console.log('Fuck you bitch');
+});
